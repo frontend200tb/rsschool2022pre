0f7098ba5f130ac02115c0b3c6eaea9c3d8cdb0b
frontend200tb <frontend200tb@gmail.com>
2022-01-22 20:30:07 +0300

    feat: add func changeClassActive

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -20,21 +20,45 @@ for (let i = 0; i < navItems.length; i++) {
 
 
 /*******************
-Portfolio images
+Portfolio change images
 *******************/
-const portfolioBtn = document.querySelector('.portfolio-btn');
 const portfolioBtns = document.querySelector('.portfolio-btns');
+const portfolioBtn = document.querySelectorAll('.portfolio-btn');
 const portfolioImages = document.querySelectorAll('.portfolio-img');
 
 function changeImage(event) {
   if(event.target.classList.contains('portfolio-btn')) {
-    let folder = event.target.dataset.season;
-    portfolioImages.forEach((img, index) => img.src = `assets/img/${folder}/${index + 1}.jpg`)
+    let season = event.target.dataset.season;
+    portfolioImages.forEach((img, index) => img.src = `assets/img/${season}/${index + 1}.jpg`);
   }
 }
 
+function changeClassActive(event) {
+  portfolioBtn.forEach((elem, index) => {
+    elem.classList.remove('btn_gold');
+  });
+  event.target.classList.add('btn_gold');
+}
+
 portfolioBtns.addEventListener('click', changeImage);
+portfolioBtns.addEventListener('click', changeClassActive);
 
 
+/*******************
+Portfolio cache images
+*******************/
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+  
+function preloadImages() {
+  seasons.forEach((season) => {
+    for(let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `assets/img/${season}/${i}.jpg`;
+      console.log(img.src);
+    }
+  })
+}
+
+preloadImages();
 
 console.log('Вёрстка соответствует макету +48\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\nНа ширине экрана 768рх и меньше реализовано адаптивное меню +22');
