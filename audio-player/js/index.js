fe2b41f5925f7c51152a7d3a0f72c23f3047fd8f
frontend200tb <frontend200tb@mail.ru>
2022-02-03 14:05:37 +0300

    feat: start js

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,1 +1,24 @@
 console.log('start');
+
+const audio = document.querySelector('.audio');
+const playBtn = document.querySelector('.play-btn');
+const playTime = document.querySelector('.play-time');
+const currentTime = document.querySelector('.current-time');
+const speakerBtn = document.querySelector('.speaker-btn');
+const volumeNum = document.querySelector('.volume-num');
+const volumeRange = document.querySelector('.volume-range');
+const leftBtn = document.querySelector('.left-btn');
+const title = document.querySelector('.title');
+const rightBtn = document.querySelector('.right-btn');
+
+console.log ('audio', audio);
+console.log ('playBtn', playBtn);
+console.log ('playTime', playTime);
+console.log ('currentTime', currentTime);
+console.log ('speakerBtn', speakerBtn);
+console.log ('volumeNum', volumeNum);
+console.log ('volumeRange', volumeRange);
+console.log ('leftBtn', leftBtn);
+console.log ('title', title);
+console.log ('rightBtn', rightBtn);
+
