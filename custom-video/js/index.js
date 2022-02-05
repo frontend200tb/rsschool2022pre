449967a7bc7f99f25f6eac7e3625088a838b4885
frontend200tb <frontend200tb@mail.ru>
2022-02-05 13:45:00 +0300

    feat: play time

--- custom-video/js/index.js
+++ custom-video/js/index.js
@@ -3,44 +3,105 @@ console.log('start');
 const player = document.querySelector('.player');
 const video = document.querySelector('video');
 const progress = document.querySelector('.progress');
-const progressBar = document.querySelector('.progress__filled');
+const currentTime = document.querySelector('.current-time');
 const playBtn = document.querySelector('.play-btn');
+const playTime = document.querySelector('.play-time');
+const trackTime = document.querySelector('.track-time');
 const ranges = document.querySelectorAll('.player__range');
 const skipBtns = document.querySelectorAll('[data-skip]');
 const log = document.querySelector('.log');
 
+
+/*******************
+Кнопка Play и время
+*******************/
 let isPlay = false;
 let currentVolume = 100;
 
+// Play / Pause
 const play = () => video.play();
 const pause = () => video.pause();
+
+// Изменение кнопки play
 const changePlayBtn = () => playBtn.classList.toggle('pause');
 const changeSpeakerBtn = () => speakerBtn.classList.toggle('mute');
 
-
-/***************
-Build out functions
-***************/
-function clickPlay() {
+// Клик на кнопке play
+const clickPlay = () => {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
 
-function skip() {
-  log.textContent = this.dataset.skip;
-  video.currentTime += parseFloat(this.dataset.skip);
-}
+// Клик на кнопке play
+playBtn.addEventListener('click', clickPlay);
 
-function changeRange() {
-  video[this.name] = this.value;
-  log.textContent = this.name;
-  log.textContent += this.value;
-}
+// Клик по видео
+video.addEventListener('click', clickPlay);
 
+video.addEventListener('play', changePlayBtn);
+video.addEventListener('pause', changePlayBtn);
+
+
+//Время равно времени трека
+const getTrackTime = () => {
+  currentTime.max = video.duration;
+  let sec_num = video.duration;
+  let hours   = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  let seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours < 10) {
+    hours   = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds; 
+  } 
+
+  trackTime.innerHTML = minutes + ':' + Math.floor(seconds);
+};
+
+video.addEventListener('loadeddata', getTrackTime);
+
+
+//функция вывода текущего времени воспроизведения
+video.ontimeupdate = function () {
+
+  var sec_num = video.currentTime;
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+  seconds=Math.round(seconds);
+
+  if (hours < 10) {
+    hours   = "0"+hours;
+  }
+  if (minutes < 10) {
+    minutes = "0"+minutes;
+  }
+  if (seconds < 10) { seconds = "0"+seconds; } playTime.innerHTML = minutes+':'+seconds; 
+  if(video.classList.contains("isPlay")) currentTime.value = video.currentTime; 
+};
+
+
+//функция для установки начала воспроизведения
+currentTime.onchange=function() { 
+
+video.pause(); video.currentTime = currentTime.value; video.play(); 
+};
+
+
+/*******************
+Progress Bar время
+*******************/
 function changeProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   log.textContent += percent;
-  progressBar.style.width = `${percent}%`;
+  currentTime.style.width = `${percent}%`;
 }
 
 function scrub(e) {
@@ -50,22 +111,30 @@ function scrub(e) {
 }
 
 
-/***************
-Hook up the event listeners
-***************/
-video.addEventListener('click', clickPlay);
-video.addEventListener('play', changePlayBtn);
-video.addEventListener('pause', changePlayBtn);
-video.addEventListener('timeupdate', changeProgress);
-
-playBtn.addEventListener('click', clickPlay);
-
-skipBtns.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', changeRange));
-ranges.forEach(range => range.addEventListener('mousemove', changeRange));
-
 let isMousedown = false
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => isMousedown && scrub(e));
 progress.addEventListener('mousedown', () => isMousedown = true);
 progress.addEventListener('mouseup', () => isMousedown = false);
+
+
+video.addEventListener('timeupdate', changeProgress);
+
+
+
+/*******************
+Кнопки 25с вперед, 10с назад
+*******************/
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function changeRange() {
+  video[this.name] = this.value;
+}
+
+skipBtns.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', changeRange));
+ranges.forEach(range => range.addEventListener('mousemove', changeRange));
+
+
