4c4a9866a976e179ff6ffd0ce1891b1b786aca11
frontend200tb <frontend200tb@gmail.com>
2022-02-06 23:12:40 +0300

    feat: add html

--- random-jokes/js/index.js
+++ random-jokes/js/index.js
@@ -1,3 +1,22 @@
 console.log('start frontend200tb random-jokes');
 
+const btn = document.querySelector(".btn");
+const content = document.querySelector(".content");
+
+async function getJock() {
+  const jokeData = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  const jokeObj = await jokeData.json();
+  content.innerHTML = jokeObj.joke;
+  console.log(jokeData);
+}
+
+document.addEventListener("DOMContentLoaded", getJock);
+
+btn.addEventListener("click", getJock);
+
+
 console.log('js code complete frontend200tb random-jokes');
