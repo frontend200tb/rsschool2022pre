6cbcdec25270a5cc3f679da53b7dffeb3963c2f5
frontend200tb <frontend200tb@gmail.com>
2022-02-17 00:17:17 +0300

    feat: add score

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -1,3 +1,70 @@
-console.log("frontend200tb tic tac toe start");
+console.log('frontend200tb tic tac toe start');
+
+/*****************
+Константы
+*****************/
+const imgX = document.querySelector('.img-x');
+console.log(imgX);
+const imgO = document.querySelector('.img-o');
+console.log(imgO);
+
+const countX = document.querySelector('.count-x');
+console.log(countX);
+const countO = document.querySelector('.count-o');
+console.log(countO);
+const playground = document.querySelector('.playground');
+console.log(playground);
+const cells = document.querySelectorAll('.cell');
+console.log(cells);
+const btn = document.querySelector('.btn');
+console.log(btn);
+
+let winX = 0;
+let winO = 0;
+let turn = 'x';
+
+/*****************
+Функции
+*****************/
+const cellOver = (e) => {
+  e.target.classList.add('highlight');
+}
+
+const cellOut = (e) => {
+  e.target.classList.remove('highlight');
+}
+
+const setWinX = () => {
+  if (winX === 9) {
+    winX = 0;
+  } else {
+  winX++;
+  }
+  countX.style.backgroundImage = `url(assets/svg/${winX}.png)`;
+};
+
+const setWinO = () => {
+  if (winO === 9) {
+    winO = 0;
+  } else {
+  winO++;
+  }
+
+  countO.style.backgroundImage = `url(assets/svg/${winO}.png)`;
+};
+
+const startGame = () => {
+  countX.style.backgroundImage = 'url(assets/svg/o.svg)';
+};
+
+/*****************
+События
+*****************/
+imgX.addEventListener("click", setWinX);
+imgO.addEventListener("click", setWinO);
+
+playground.addEventListener('mouseover', cellOver);
+playground.addEventListener('mouseout', cellOut);
+btn.addEventListener("click", setWinX);
 
 console.log("frontend200tb tic tac toe finish");
