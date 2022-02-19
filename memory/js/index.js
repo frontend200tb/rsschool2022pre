d5d99e01c30466cfb8ae86167bab013ba966edd6
frontend200tb <frontend200tb@gmail.com>
2022-02-19 21:45:53 +0300

    feat: fruits

--- memory/js/index.js
+++ memory/js/index.js
@@ -4,7 +4,10 @@ console.log('frontend200tb memory start');
 /*****************
 Константы
 *****************/
+const placeCount = document.querySelector('.place-count');
+const count = document.querySelector('.count');
 const cards = document.querySelectorAll('.card');
+const btn = document.querySelector('.btn');
 
 
 /*****************
@@ -14,6 +17,11 @@ let hasFlippedCard = false;
 let lockCard = false;
 let firstCard;
 let secondCard;
+let currentScore = 0;
+let countOpenCards = 0;
+let countAllCards = 16;
+let records = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let nextRecord = 0;
 
 
 /*****************
@@ -27,26 +35,35 @@ function flipCard() {
   if (this === firstCard) {
     return
   }
-
+  
+  currentScore++;
+  count.textContent = currentScore + ' moves';
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return;
   }
 
   secondCard = this;
+  lockCard = true;
   
-  checkForMatch();  
+  checkForMatch();
 }
 
 function checkForMatch() {
-  if (firstCard.dataset.img === secondCard.dataset.img) {
+  let isMatch = firstCard.dataset.img === secondCard.dataset.img;
+  if (isMatch) {
     disableCards();
-    return;
-  }
-  
-  unflipCards();
+    countOpenCards += 2;
+    if (countOpenCards === countAllCards) {
+      finishGame();
+    }
+
+  } else {
+    unflipCards();
+  } 
 }
   
 function disableCards() {
@@ -57,23 +74,59 @@ function disableCards() {
 }
   
 function unflipCards() {
-  lockCard = true;
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-    lockCard = false;
-  }, 1500);
+
+    resetBoard();
+  }, 1000);
 }
 
 function resetBoard() {
-  [hasFlippedCard, lockCard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+  hasFlippedCard = false;
+  lockCard = false;
+  firstCard = null;
+  secondCard = null;
 }
+
+function shuffle() {
+  cards.forEach(card => {
+    let ramdomPos = Math.floor(Math.random() * 12);
+    card.style.order = ramdomPos;
+  });
+}
+
+const startGame = () => {
+  cards.forEach(card => {
+    card.classList.remove('flip');
+  });
+
+  cards.forEach(elem => elem.addEventListener('click', flipCard));
+
+  resetBoard();
+  setTimeout(() => {  
+    shuffle();
+  }, 1000);
+  currentScore = 0;
+  count.textContent = currentScore + ' moves';
+
+}
+
+const finishGame = () => {
+  placeCount.textContent = currentScore;
+  count.textContent = 'finish';
+}
+
+/*****************
+Local storage
+*****************/
+
 
 
 /*****************
 События
 *****************/
-cards.forEach(card => card.addEventListener('click', flipCard));
+document.addEventListener('DOMContentLoaded', startGame)
+btn.addEventListener('click', startGame);
 
 console.log('frontend200tb memory finish');
